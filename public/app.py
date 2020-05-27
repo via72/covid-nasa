594ccca665ba9e2fdbefa9e146aa21dbe43b18f2
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-05-27 16:20:13 +0530

    render template

--- public/app.py
+++ public/app.py
@@ -1,7 +1,7 @@
-from flask import Flask 
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1> Hello </h1>"
+    return render_template("index.html")
