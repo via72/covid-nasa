d7dbea1f4c960dc91f7a4e833c2c35533383046b
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-05-30 10:59:25 +0530

    favicon added

--- public/app.py
+++ public/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'images/icon.ico')
 @app.route("/")
 def home():
     return render_template("index.html")
